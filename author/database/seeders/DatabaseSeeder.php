8ad17936c7d2307200d35e753730789f29fbfd8a
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-27 15:25:39 +0430

    Author Component: Create author seeder

--- author/database/seeders/DatabaseSeeder.php
+++ author/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Author;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        Author::factory()
+            ->count(50)
+            ->create();
     }
 }
