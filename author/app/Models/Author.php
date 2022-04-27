b3eae629484ac535fba4f039a3c1c8b37c92c6f1
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-27 13:37:20 +0430

    Author component: Add author model

---
+++ author/app/Models/Author.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'gender',
+        'country',
+    ];
+
+    const GENDER = [
+        "male" => "male",
+        "female" => "female",
+    ];
+}
