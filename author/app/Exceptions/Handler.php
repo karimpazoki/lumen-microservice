7d4e8ba7adc56cf8fb2f715c6d753162cc880634
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-28 14:22:07 +0430

    Author component: Improve Exception handling

--- author/app/Exceptions/Handler.php
+++ author/app/Exceptions/Handler.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+use Illuminate\Http\Response;
+
+use App\Traits\ResponseBuilder;
 
 class Handler extends ExceptionHandler
 {
+    use ResponseBuilder;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +54,29 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        // dd(get_class($exception));
+        if($exception instanceof NotFoundHttpException) {
+            $message = "404 Not Found";
+            return $this->error($message, Response::HTTP_NOT_FOUND);
+        }
+        if($exception instanceof ModelNotFoundException) {
+            $model = strtolower(last(explode("\\",$exception->getModel())));
+            $message = "this {$model} does not exists";
+            return $this->error($message, Response::HTTP_NOT_FOUND);
+        }
+        if($exception instanceof AuthorizationException) {
+            $message = $exception->getMessage();
+            return $this->error($message, Response::HTTP_FORBIDDEN);
+        }
+        if($exception instanceof AuthenticationException) {
+            $message = $exception->getMessage();
+            return $this->error($message, Response::HTTP_UNAUTHORIZED);
+        }
+        if($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->error($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         return parent::render($request, $exception);
     }
 }
